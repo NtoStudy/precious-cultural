4ef61104329be95d48851eb83240a9e68262ecfa
NtoStudy <2943343011@qq.com>
2024-09-25 18:53:18 +0800

    完善首页背景图随页面大小变化而变化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, watchEffect} from 'vue';
 // 导入图片资源 作为轮播图
 import bg1 from '@/assets/bg1.jpg';
 import bg2 from '@/assets/bg2.jpg';
@@ -10,12 +10,34 @@ const images = [bg1, bg2, bg3];
 const interval = ref(3000);
 // 控制背景图为视口高度减去60px
 const carouselHeight = ref('100vh');
-
+const viewportHeight = ref(window.innerHeight);
+// 监听视口高度变化，动态设置轮播图高度
 onMounted(() => {
   // 使用window.innerHeight获取视口高度
-  const viewportHeight = window.innerHeight;
   carouselHeight.value = `calc(${viewportHeight}px - 60px)`;
 })
+
+// 页面发生变化的时候 动态设置高度
+// 监听viewportHeight变化并根据窗口高度调整carousel高度
+watchEffect(() => {
+  // 监听viewportHeight变化
+  if (viewportHeight.value !== window.innerHeight) {
+    viewportHeight.value = window.innerHeight;
+    carouselHeight.value = `calc(${viewportHeight.value}px - 60px)`;
+  }
+});
+// 监听窗口大小变化事件
+window.addEventListener('resize', () => {
+  viewportHeight.value = window.innerHeight;
+  carouselHeight.value = `calc(${viewportHeight.value}px - 60px)`;
+});
+// 监听方向变化事件
+window.addEventListener('orientationchange', () => {
+  viewportHeight.value = window.innerHeight;
+  carouselHeight.value = `calc(${viewportHeight.value}px - 60px)`;
+});
+
+
 </script>
 
 <template>
