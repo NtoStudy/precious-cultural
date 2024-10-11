be3d047d779a17499aafb685b9e24eb351017b4f
NtoStudy <2943343011@qq.com>
2024-10-11 13:42:12 +0800

    写入用户端的api接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
 // 登录接口
 import requests from "@/http/index.js";
 
+/**
+ * 用户登录接口
+ * @param username
+ * @param password
+ * @returns {*}
+ */
 export const userLoginApi = (username, password) => requests({
     url: '/user/login',
     method: 'post',
@@ -10,11 +16,47 @@ export const userLoginApi = (username, password) => requests({
     }
 })
 
-// export const userLoginApiAA = (username, password) => requests({
-//     url: '/user/login',
-//     method: 'post',
-//     data:{
-//         username: username,
-//         password: password
-//     }
-// })
+/**
+ * 根据非遗id查询全景图
+ * @param id
+ * @returns {*}
+ */
+export const userPanoramaByIdApi = (id) => requests({
+    url:`/user/panorama/${id}`,
+    method: 'get'
+})
+
+
+/**
+ * 根据id查询指定非遗信息
+ * @param id
+ * @returns {*}
+ */
+export const userNonHeritageByIdApi = (id) => requests({
+    url:`/user/nonHeritage/${id}`,
+    method: 'get'
+})
+
+/**
+ * 分页查询用户非遗信息
+ * @returns {*}
+ */
+export const userNonHeritagePageApi = () => requests({
+    url:'/user/nonHeritage/page',
+    method: 'get'
+})
+
+/**
+ * 根据非遗id查询非遗图
+ * @param id
+ * @returns {*}
+ */
+export const userNonHeritageImgByIdApi = (id) => requests({
+    url:`/user/nonHeritageImg/${id}`,
+    method: 'get'
+})
+
+export const userInheritorsByIdApi = (id)=> requests({
+    url:`/user/inheritors/${id}`,
+    method: 'get'
+})
