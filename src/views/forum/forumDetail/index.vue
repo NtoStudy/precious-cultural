f16edd857f734131f47dbcb43592dbebf47a2613
NtoStudy <158829124+NtoStudy@users.noreply.github.com>
2024-10-22 19:37:49 +0800

    完成论坛页面的基本样式

--- src/views/forum/forumDetail/index.vue
+++ src/views/forum/forumDetail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
-import LastPage from  "@/components/lastPage.vue";
 import {ref,defineProps} from "vue";
+import comment from "./comment/index.vue"
+import articleContent from "./articleContent/index.vue"
 const props = defineProps({
   articleId: {
     type: Number,
@@ -48,40 +49,75 @@ const AddLike = () => {
   }
   data.value.isLiked = !data.value.isLiked; // 切换状态
 }
+const contents = ref(['这是第一个大标题','这是第二个大标题','这是第三个大标题','这是第四个大标题','这是第五个大标题'])
+const judgeContent = ref(true)
+const updateContent = () => {
+  if(judgeContent.value){
+    contents.value = []
+  }else{
+    contents.value = ['这是第一个大标题','这是第二个大标题','这是第三个大标题','这是第四个大标题','这是第五个大标题']
+  }
+  judgeContent.value = !judgeContent.value; // 切换状态
+}
+
 </script>
 <template>
-  <LastPage />
   <div class="layout-container">
-    <el-card class="article-card" style="width: 74%;">
-      <h1 class="article-title">{{data.title}}</h1>
-      <p class="article-meta">作者: {{data.author}} · {{data.date}}</p>
-      <div class="article-content">
-        <div v-for="(item,index) in data.Content" :key="index">
-          <h3>{{item.smaTitle}}</h3>
-          <p>{{item.content}}</p>
+    <div style="width: 74%;">
+      <el-card class="article-card">
+        <h1 class="article-title">{{data.title}}</h1>
+        <p class="article-meta">作者: {{data.author}} · {{data.date}}</p>
+        <article-content />
+      </el-card>
+      <comment moment-id="123" post-add-comment-form="[{wq:123},{wq:123},{wq:123}]"/>
+    </div>
+    <div class="author-container" style="width: 24%;">
+      <el-card class="author-card" style="width: 100%;">
+        <div class="author-info">
+          <img src="https://via.placeholder.com/50" alt="Avatar" class="author-avatar" />
+          <div>
+            <h3 style="margin-bottom: 5px;">{{data.author}}</h3>
+            <p>年度资深创作者</p>
+          </div>
         </div>
-      </div>
-    </el-card>
-
-    <el-card class="author-card" style="width: 22%;">
-      <div class="author-info">
-        <img src="https://via.placeholder.com/50" alt="Avatar" class="author-avatar" />
+        <div class="author-stats">
+          <div class="author-content">
+            <p>8 文章</p>
+            <p>10k 阅读</p>
+            <p>{{data.like}} 粉丝</p></div>
+          <div class="buttons">
+            <el-button type="primary" @click="AddLike" style="height: 40px; width: 100px;">{{data.isLiked ? '取消关注' : '关注'}}</el-button>
+            <el-button size="large" style="height: 40px; width: 100px;">私信</el-button>
+          </div>
+        </div>
+      </el-card>
+      <el-card calss="contents-card" style="margin-top: 30px;">
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+          <h2>目录</h2>
+          <div @click="updateContent()" style="cursor: pointer;">
+            <p v-if="judgeContent">收起 <el-icon><ArrowUp /></el-icon></p>
+            <p v-else>展开 <el-icon><ArrowDown /></el-icon></p>
+          </div>
+        </div>
+        <el-divider style="margin: 16px 0"/>
+        <div v-for="(item,index) in contents" :key="index" style="margin-bottom: 16px; margin-left: 5px;">
+          <p>{{item    }}</p>
+        </div>
+      </el-card>
+      <el-card calss="contents-card" style="margin-top: 30px;">
         <div>
-          <h3 style="margin-bottom: 5px;">{{data.author}}</h3>
-          <p>年度资深创作者</p>
+          <h2>相关文章</h2>
         </div>
-      </div>
-      <div class="author-stats">
-        <div class="author-content">
-          <p>8 文章</p>
-          <p>10k 阅读</p>
-          <p>{{data.like}} 粉丝</p></div>
-        <div class="buttons">
-          <el-button type="primary" @click="AddLike" style="height: 40px; width: 100px;">{{data.isLiked ? '取消关注' : '关注'}}</el-button>
-          <el-button size="large" style="height: 40px; width: 100px;">私信</el-button>
+        <el-divider style="margin: 16px 0"/>
+        <div v-for="(item,index) in contents" :key="index" style="margin-bottom: 16px; margin-left: 5px; cursor: pointer;">
+          <h3 style="margin-bottom: 10px; ">这是文章标题</h3>
+          <div style="display: flex; color: #666;">
+            <p style="margin-right: 16px;">阅读量123 · </p>
+            <p>点赞量31</p>
+          </div>
         </div>
-      </div>
-    </el-card>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -91,6 +127,7 @@ const AddLike = () => {
   display: flex;
   padding: 0 100px;
   justify-content: space-between;
+  margin-top: 30px;
 
   .article-card {
 
@@ -125,48 +162,54 @@ const AddLike = () => {
       }
     }
   }
-  .author-card {
-    height: 200px;
-    .author-info {
-      display: flex;
-      align-items: center;
-      margin-bottom: 20px;
+  .author-container{
+    .author-card {
+      height: 200px;
+      .author-info {
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
 
 
-      .author-avatar {
-        border-radius: 50%;
-        margin-right: 12px;
+        .author-avatar {
+          border-radius: 50%;
+          margin-right: 12px;
+        }
+
+        h3 {
+          margin: 0;
+          font-size: 18px;
+
+        }
+
+        p {
+          color: #666;
+        }
       }
 
-      h3 {
-        margin: 0;
-        font-size: 18px;
+      .author-stats {
+        .author-content{
+          display: flex;
+          justify-content: space-between;
+          font-size: 16px;
+        }
+        p {
+          margin: 5px 0;
+          font-weight: bold;
+        }
 
-      }
-
-      p {
-        color: #666;
+        .buttons {
+          margin-top: 10px;
+          display: flex;
+          justify-content: space-between;
+        }
       }
     }
-
-    .author-stats {
-      .author-content{
-        display: flex;
-        justify-content: space-between;
-        font-size: 16px;
-      }
-      p {
-        margin: 5px 0;
-        font-weight: bold;
-      }
-
-      .buttons {
-        margin-top: 10px;
-        display: flex;
-        justify-content: space-between;
-      }
+    .contents-card{
+      margin-top: 30px;
     }
   }
+
 }
 
 
